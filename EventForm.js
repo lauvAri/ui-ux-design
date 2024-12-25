3f0b3a71142610c2e3eb7386c7c13236a9239a81
lauv ari <198719993@qq.com>
2024-12-25 21:02:27 +0800

    update EventForm.js

--- EventForm.js
+++ EventForm.js
@@ -1,49 +1,57 @@
 // EventFormComponent.js
+import majors from './majors.js';
+import schools from './schools.js';
 
 // 定义 EventForm 组件
 const EventForm = {
   template: `
-    <el-form ref="form" :model="form" label-width="80px">
-      <el-form-item label="学生姓名">
-        <el-input v-model="form.name"></el-input>
-      </el-form-item>
-      <el-form-item label="学生性别">
-        <el-select v-model="form.gender" placeholder="选择性别">
-          <el-option label="男" value="male"></el-option>
-          <el-option label="女" value="female"></el-option>
-        </el-select>
-      </el-form-item>
-      <el-form-item label="入学时间">
-        <el-col :span="11">
-          <el-date-picker type="date" placeholder="选择日期" v-model="form.date1" style="width: 100%;"></el-date-picker>
-        </el-col>
-        <el-col class="line" :span="2">-</el-col>
-        <el-col :span="11">
-          <el-time-picker placeholder="选择时间" v-model="form.date2" style="width: 100%;"></el-time-picker>
-        </el-col>
-      </el-form-item>
-      <el-form-item label="学号">
-        <el-input  v-model="form.studentId"></el-input>
-      </el-form-item>
-      <el-form-item label="电话号码">
-        <el-input  v-model="form.mobile"></el-input>
-      </el-form-item>
-      <el-form-item label="特殊资源">
-        <el-radio-group v-model="form.resource">
-          <el-radio label="线上品牌商赞助"></el-radio>
-          <el-radio label="线下场地免费"></el-radio>
-        </el-radio-group>
-      </el-form-item>
-      <el-form-item label="活动形式">
-        <el-input type="textarea" v-model="form.desc"></el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="onSubmit">立即创建</el-button>
-        <el-button>取消</el-button>
-      </el-form-item>
-    </el-form>
+  <el-form ref="form" :model="form" label-width="80px">
+    <el-form-item label="学生姓名">
+      <el-input v-model="form.name"></el-input>
+    </el-form-item>
+    <el-form-item label="学生性别">
+      <el-select v-model="form.gender" placeholder="选择性别">
+        <el-option label="男" value="male"></el-option>
+        <el-option label="女" value="female"></el-option>
+      </el-select>
+    </el-form-item>
+    <el-form-item label="入学时间">
+      <el-col :span="11">
+        <el-date-picker type="date" placeholder="选择日期" v-model="form.date1" style="width: 100%;"></el-date-picker>
+      </el-col>
+      <el-col class="line" :span="2">-</el-col>
+      <el-col :span="11">
+        <el-time-picker placeholder="选择时间" v-model="form.date2" style="width: 100%;"></el-time-picker>
+      </el-col>
+    </el-form-item>
+    <el-form-item label="学号">
+      <el-input  v-model="form.studentId"></el-input>
+    </el-form-item>
+
+    <el-form-item label="电话号码">
+      <el-input  v-model="form.mobile"></el-input>
+    </el-form-item>
+
+    <el-form-item label="学院">
+      <el-select v-model="form.school" placeholder="选择学院" filterable>
+        <el-option v-for="item in schools" :key="item.value" :label="item.label" :value="item.value"></el-option>
+      </el-select>
+    </el-form-item>
+
+    <el-form-item label="专业">
+      <el-select v-model="form.major" placeholder="选择专业" filterable>
+        <el-option v-for="item in majorsFiltered" :key="item.value" :label="item.label" :value="item.value"></el-option>
+      </el-select>
+    </el-form-item>
+
+    <el-form-item>
+      <el-button type="primary" @click="onSubmit">立即创建</el-button>
+      <el-button>取消</el-button>
+    </el-form-item>
+  </el-form>
   `,
   data() {
+    console.log('majors', majors);
     return {
       form: {
         name: '',
@@ -51,11 +59,18 @@ const EventForm = {
         date1: '',
         date2: '',
         studentId: '',
-        type: [],
-        resource: '',
-        desc: ''
-      }
+        mobile: '',
+        school: '',
+        major: '',
+      },
+      majors,
+      schools,
     };
+  },
+  computed: {
+    majorsFiltered() {
+      return this.majors.filter(major => major.school === this.form.school);
+    }
   },
   methods: {
     onSubmit() {
