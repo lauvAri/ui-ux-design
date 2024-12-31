0c3877f11704a648ea298bbde75def10aee9d7f4
ONeofSu <148432545+ONeofSu@users.noreply.github.com>
2025-01-01 02:32:24 +0800

    2025/1/1 2:32
    基本全部完成 进入debug环节

--- account/account.js
+++ account/account.js
@@ -1,3 +1,16 @@
+//设置当前登录账号
+function setNowLoginUser(username){
+    localStorage.setItem('nowLoginUser', username);
+}
+
+function getNowLoginUser(){
+    return localStorage.getItem('nowLoginUser');
+}
+
+function clearNowLoginUser() {
+    localStorage.removeItem('nowLoginUser');
+}
+
 // 存储账号数据
 function saveAccountData(username, password) {
     // 获取现有账号数据（如果存在）
@@ -7,9 +20,8 @@ function saveAccountData(username, password) {
     const newAccount = {
         username: username,
         password: password,
-        admin: null,
-        avatar: null,
-        tel: null
+        admin: "admin",
+        tel: "",
     };
 
     // 将新账号添加到数组中
@@ -27,6 +39,51 @@ function getAccountData() {
     return accounts;
 }
 
+//读取单个账号数据
+function getAccount(username){
+    // 获取现有账号数据
+    let accounts = JSON.parse(localStorage.getItem('accounts')) || [];
+
+    // 查找要更新的账号
+    const account = accounts.find(acc => acc.username === username);
+
+    return account;
+}
+
+//获得账号密码
+function getAccountPass(username){
+    const account = getAccount(username);
+    return account.password;
+}
+
+//获得账号手机号
+function getAccountTel(username){
+    const account = getAccount(username);
+    return account.tel;
+}
+
+//获得账号用户名 
+function getAccountAdmin(username){
+    const account = getAccount(username);
+    return account.admin;
+}
+
+//更新账号数据
+function updateAccountData(newAccount){
+    let accounts = JSON.parse(localStorage.getItem('accounts')) || [];
+    const index = accounts.findIndex(account => account.username === newAccount.username);
+
+    if (index !== -1) {
+        // 更新账号数据
+        accounts[index] = { ...accounts[index], ...newAccount };
+        // 保存更新后的数据
+        localStorage.setItem('accounts', JSON.stringify(accounts));
+        console.log('账号数据已更新');
+    } else {
+        console.error('账号未找到');
+    }
+}
+
 // 查看特定用户名是否被占用
 function isUserExist(username){
     // 获取现有账号数据
@@ -41,7 +98,7 @@ function isUserExist(username){
 }
 
 // 更新特定账号数据
-function updateAccountData(oldUsername, newPassword) {
+function updateAccountPass(oldUsername, newPassword) {
     // 获取现有账号数据
     let accounts = JSON.parse(localStorage.getItem('accounts')) || [];
 
@@ -89,5 +146,15 @@ function isAccountCorrect(username,password){
         }
     }
 
+    return false;
+}
+
+function isTelLength11(value){
+    if(!value){
+        return false;
+    }
+    if(value.length == 11){
+        return true;
+    }
     return false;
 }
--- account/accountInfo.js
+++ account/accountInfo.js
@@ -3,86 +3,306 @@ const accountInfo = {
     <div style="width:75%;position:relative;left:12.5%">
     <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" class="demo-ruleForm" label-position="left" 
     style="width: 100%; border:2px solid #409EFF;border-radius: 30px;">
+    <br/>
     <div class="btn" style="display: flex; justify-content: center; align-items: center; font-size:20px">
         <span>账号信息</span>
     </div>
-    <div style="position:relative; left:8%; top:20px; width:90%">
+    <div style="position:relative; left:30%; top:20px; width:80%">
         <el-row>
-            <el-col :span="12">
+            <el-col :span="24">
             <div class="username" style="position: relative;">
                 <i class="el-icon-monitor" style="font-size: 20px;"></i>
                 <span style="font-size: 20px;">账号:</span>&nbsp&nbsp&nbsp&nbsp
                 <span id="usernameSpan">username</span>
             </div>
             </el-col>
-            <el-col :span="12">
+        </el-row>
+        <br/>
+        <el-row>
+            <el-col :span="24">
             <div class="admin" style="position: relative;">
                 <i class="el-icon-user" style="font-size: 20px;"></i>
                 <span style="font-size: 20px;">用户名:</span>
-                <el-input v-model="ruleForm.adminInput" :disabled="isAdminIptDisable" style="width: 40%;" id="adminIpt"></el-input>
-                <el-button type="primary" @click="updateAdmin"><span id="updateAdminSpan">修改</span></el-button>
+                <el-form-item prop="adminInput">
+                <el-input v-model="ruleForm.adminInput" :disabled="isAdminIptDisable" style="width: 50%;" id="adminIpt"></el-input>
+                </el-form-item>
             </div>
             </el-col>
         </el-row>
         <br/>
         <el-row>
-            <el-col :span="12">
-            <div class="pass" style="position: relative;">
-                <i class="el-icon-lock" style="font-size: 20px;"></i>
-                <span style="font-size: 20px;">密码:</span>&nbsp&nbsp&nbsp&nbsp
-                <el-input v-model="ruleForm.passInput" :disabled="isPassIptDisable" type="password" style="width: 40%;" 
-                id="passIpt" autocomplete="off"></el-input>
-                <el-button type="primary" @click="updatePass" id="passBtn"><span id="updatePassSpan">修改</span></el-button>
-            </div>
-            </el-col>
-            <el-col :span="12">
-            <div class="checkPass" style="position: relative;" id="checkPass" v-show="isCheckpassVisible">
-                <span style="font-size: 20px;">确认密码:</span>&nbsp
-                <el-input v-model="ruleForm.checkpassInput" type="password" style="width: 40%;" id="checkpassIpt" autocomplete="off"></el-input>
-            </div>
-            </el-col>
-        </el-row>
-        <br/>
-        <el-row>
-            <el-col :span="12">
+            <el-col :span="24">
             <div class="tel" style="position: relative;">
                 <i class="el-icon-phone-outline" style="font-size: 20px;"></i>
                 <span style="font-size: 20px;">电话:</span>&nbsp&nbsp&nbsp&nbsp
-                <el-input v-model="ruleForm.telInput" :disabled="isTelIptDisable" type="tel" style="width: 40%;" 
-                id="telIpt" autocomplete="off"></el-input>
+                <el-form-item prop="telInput">
+                <el-input v-model="ruleForm.telInput" :disabled="isTelIptDisable" type="tel" style="width: 50%;" 
+                id="telIpt" autocomplete="off" ref="telInput"></el-input>
+                </el-form-item>
             </div>
             </el-col>
         </el-row>
+        <br/>
+        <el-row>
+            <el-col :span="24">
+            <div class="pass" style="position: relative;">
+                <i class="el-icon-lock" style="font-size: 20px;"></i>
+                <span style="font-size: 20px;">密码:</span>&nbsp&nbsp&nbsp&nbsp
+                <el-form-item prop="passInput">
+                <el-input v-model="ruleForm.passInput" :disabled="isPassIptDisable" type="password" style="width: 50%;" 
+                id="passIpt" autocomplete="off" ref="passInput"></el-input>
+                </el-form-item>
+            </div>
+            </el-col>
+        </el-row>
+        <br/>
+        <el-row>
+            <el-col :span="24">
+            <div class="checkPass" style="position: relative;" id="checkPass" v-show="isCheckpassVisible">
+                <span style="font-size: 20px;">确认密码:</span>&nbsp
+                <el-form-item prop="checkpassInput">
+                <el-input v-model="ruleForm.checkpassInput" type="password" style="width: 50%;" id="checkpassIpt" 
+                autocomplete="off" ref="checkpassInput"></el-input>
+                </el-form-item>
+            </div>
+            </el-col>
+        </el-row>
+        <br/>
     </div>
         <br/>
         <el-row>
             <el-col :span="24">
             <div class="btn" style="display: flex; justify-content: center; align-items: center;">
-                <el-button type="primary" @click="updatePass" id="passBtn"><span id="updatePassSpan">修改</span></el-button>
+                <el-button type="primary" @click="updateBtn" id="updateBtn" style="width:100px;">
+                    <span id="updateSpan" style="display: flex; justify-content: center; align-items: center;">修 改</span>
+                </el-button>
+                <el-button type="primary" @click="exitBtn" id="exitBtn" style="width:100px;" :disabled="isExitDisable">
+                    <span id="exitSpan"  style="display: flex; justify-content: center; align-items: center;">退 出 登 录</span>
+                </el-button>
+                <el-button type="primary" @click="deleteBtn" id="deleteBtn" style="width:100px;" :disabled="isDeleteDisable">
+                    <span id="deleteSpan" style="display: flex; justify-content: center; align-items: center;">注 销 账 号</span>
+                </el-button>
             </div>
             </el-col>
         </el-row>
         <br/>
     </el-form>
+        <el-dialog
+            title="提示"
+            :visible.sync="successDialogVisible"
+            width="30%"
+            :before-close="handleClose">
+            <span>修改成功</span>
+            <span slot="footer" class="dialog-footer">
+                <el-button type="primary" @click="successDialogVisible = false;">确 定</el-button>
+            </span>
+        </el-dialog>
+
+        <el-dialog
+            title="提示"
+            :visible.sync="failDialogVisible"
+            width="30%"
+            :before-close="handleClose">
+            <span>修改内容不符合要求，请重试</span>
+            <span slot="footer" class="dialog-footer">
+            <el-button type="primary" @click="cancelUpdate();failDialogVisible = false">取 消 修 改</el-button>
+                <el-button type="primary" @click="failDialogVisible = false" id="failBtnS">确 定</el-button>
+            </span>
+        </el-dialog>
+
+        <el-dialog
+            title="提示"
+            :visible.sync="telDialogVisible"
+            width="30%"
+            :before-close="handleClose">
+            <span>您当前尚未绑定手机，确定继续吗？</span>
+            <span slot="footer" class="dialog-footer">
+                <el-button type="primary" @click="telDialogVisible = false">取 消</el-button>
+                <el-button type="primary" @click="updateData();telDialogVisible = false">确 定</el-button>
+            </span>
+        </el-dialog>
+
+        <el-dialog
+            title="提示"
+            :visible.sync="exitDialogVisible"
+            width="30%"
+            :before-close="handleClose">
+            <span>确定退出账号吗?</span>
+            <span slot="footer" class="dialog-footer">
+                <el-button type="primary" @click="exitDialogVisible = false">取 消</el-button>
+                <el-button type="primary" @click="exit">确 定</el-button>
+            </span>
+        </el-dialog>
+
+        <el-dialog
+            title="警告"
+            :visible.sync="confirmDialogVisible"
+            width="30%"
+            :before-close="handleClose">
+            <span>您正在进行注销账号操作,是否继续?</span>
+            <span slot="footer" class="dialog-footer">
+                <el-button type="primary" @click="confirmDialogVisible = false">取 消</el-button>
+                <el-button type="primary" @click="deleteAccount" id="confirmBtn">确 定</el-button>
+            </span>
+        </el-dialog>
+
+        <el-dialog
+            title="提示"
+            :visible.sync="deleteDialogVisible"
+            width="30%"
+            :before-close="handleClose">
+            <span>注销账号会删除账号所有数据，确定注销账号吗?</span>
+            <span slot="footer" class="dialog-footer">
+                <el-button type="primary" @click="deleteDialogVisible = false">取 消</el-button>
+                <el-button type="primary" @click="deleteDialogVisible = false;confirmDialogVisible = true" id="deleteBtn">确 定</el-button>
+            </span>
+        </el-dialog>
+
     </div>
     `,
     data(){
+        var validateUser = (rule,value,callback) => {
+            if (value === '') {
+                callback(new Error('请输入用户名'));
+            }else {
+                callback();
+            }
+        }
+        var validateTel = (rule, value, callback) => {
+            if(!isTelLength11(value) && value != ""){
+                callback(new Error('请输入正确的手机号码'));
+            }else{
+                callback();
+            }
+        }
+        var validatePass = (rule, value, callback) => {
+            if (value === '') {
+            callback(new Error('请输入密码'));
+            }else if(!isRegisterPasswordValid(value)){
+                callback(new Error('密码至少为5位且应包含字母以及数字'))
+            }else {
+            if (this.ruleForm.checkPass !== '') {
+                this.$refs.ruleForm.validateField('checkPass');
+            }
+            callback();
+            }
+        }
+        var validatePass2 = (rule, value, callback) => {
+            if (value === '') {
+            callback(new Error('请再次输入密码'));
+            } else if (value != this.ruleForm.passInput) {
+            callback(new Error('两次输入密码不一致!'));
+            } else {
+            callback();
+            }
+        }
         return{
             isAdminIptDisable: true,
             isPassIptDisable: true,
             isTelIptDisable: true,
             isCheckpassVisible: false,
+            isUpdatingFlag: false,
+            isExitDisable: false,
+            isDeleteDisable: false,
+            successDialogVisible: false,
+            failDialogVisible: false,
+            telDialogVisible: false,
+            exitDialogVisible: false,
+            deleteDialogVisible: false,
+            confirmDialogVisible: false,
+
             ruleForm:{
                 adminInput: 'aa',
                 passInput: 'pass1',
                 checkpassInput: '',
+                telInput: '',
             },
+            rules: {
+                adminInput: [
+                    {validator: validateUser, trigger: 'blur'}
+                ],
+                telInput: [
+                    {validator: validateTel, trigger: 'blur'}
+                ],
+                passInput: [
+                    { validator: validatePass, trigger: 'blur' }
+                ],
+                checkpassInput: [
+                    { validator: validatePass2, trigger: 'blur' }
+                ],
+            }
         };
     },
-    mount(){
-        
+    mounted(){
+        document.addEventListener('keydown',this.keydownOccured);
+        this.initInfo();
     },
     methods: {
+        //初始化信息
+        initInfo(){
+            let accountUsername = getNowLoginUser();
+            let account = getAccount(accountUsername);
+            document.getElementById('usernameSpan').innerText = account.username;
+            this.ruleForm.adminInput = account.admin;
+            this.ruleForm.passInput = account.password;
+            this.ruleForm.telInput = account.tel;
+        },
+        //修改按钮被点击
+        updateBtn(){
+            if(!this.isUpdatingFlag){
+                this.isAdminIptDisable = false;
+                this.isPassIptDisable = false;
+                this.isCheckpassVisible = true;
+                this.isTelIptDisable = false;
+                this.isUpdatingFlag = true;
+                this.isExitDisable = true;
+                this.isDeleteDisable = true;
+                document.getElementById("updateSpan").innerText = "完 成";
+            }else if(this.isUpdatingFlag){
+                this.checkIsValid();
+            }      
+        },
+        updateData(){
+            let newAccount = {
+                username : getNowLoginUser(),
+                password : this.ruleForm.passInput,
+                admin : this.ruleForm.adminInput,
+                tel : this.ruleForm.telInput,
+            }
+            updateAccountData(newAccount);
+
+            console.log("更新");
+            this.successDialogVisible = true;
+            this.isUpdatingFlag = false;
+            this.isExitDisable = false;
+            this.isDeleteDisable = false;
+            this.isAdminIptDisable = true;
+            this.isPassIptDisable = true;
+            this.isCheckpassVisible = false;
+            this.isTelIptDisable = true;
+            this.checkpassInput = "";
+            document.getElementById("updateSpan").innerText = "修 改";
+        },
+        cancelUpdate(){
+            this.initInfo();
+            this.isUpdatingFlag = false;
+            this.isAdminIptDisable = true;
+            this.isPassIptDisable = true;
+            this.isCheckpassVisible = false;
+            this.isTelIptDisable = true;
+            this.isExitDisable = false;
+            this.isDeleteDisable = false;
+            this.checkpassInput = "";
+            document.getElementById("updateSpan").innerText = "修 改";
+        },
+        deleteBtn(){
+            this.deleteDialogVisible = true;
+            document.getElementById('deleteBtn').focus();
+        },
+        deleteAccount(){
+            deleteAccountData(getNowLoginUser());
+            window.location.href = "./login.html";
+        },
         //更新用户名
         updateAdmin(){
             if(this.isAdminIptDisable){
@@ -106,6 +326,96 @@ const accountInfo = {
                 document.getElementById("passBtn").style.visibility = "visible";
                 this.isCheckpassVisible = false;
             }
+        },
+        isUpdateValid(){
+            console.log("isUpdateValid");
+            if(this.ruleForm.adminInput != "" && isRegisterPasswordValid(this.ruleForm.passInput) && 
+            isRegisterCheckpassValid(this.ruleForm.passInput,this.ruleForm.checkpassInput)){
+                console.log(this.ruleForm.passInput);
+                console.log(this.ruleForm.checkpassInput);
+                return true;
+            }
+            return false
+        },
+        checkIsValid(){
+            if(this.isUpdateValid() && this.ruleForm.telInput == ""){
+                this.telDialogVisible = true;
+            }else if(this.isUpdateValid() && isTelLength11(this.ruleForm.telInput)){
+                this.updateData();
+            }else{
+                this.failDialogVisible = true;
+                document.getElementById('failBtn').focus();
+            }
+        },
+        exitBtn(){
+            this.exitDialogVisible = true;
+        },
+        exit(){
+            window.location.href = "./login.html";
+        },
+        handleClose(){
+
+        },
+        isDialogVisible(){
+            if(this.successDialogVisible || this.failDialogVisible || this.telDialogVisible || 
+                this.exitDialogVisible || this.deleteDialogVisible || this.confirmDialogVisible){
+                    return true;
+            }
+            return false;
+        },
+        keydownOccured(event){
+            if(event.key === 'Enter'){
+                console.log(this.isDialogVisible());
+                if(!this.isDialogVisible()){
+                    //console.log("Enter pressed")
+                    if (document.activeElement.id === "adminIpt") {  //焦点在用户名框 按enter到tel框
+                    event.preventDefault(); // 阻止默认行为（表单提交等）
+                    //console.log("Enter pressed on username input, focusing password input.");
+
+                    // 将焦点设置到tel输入框
+                    this.$nextTick(() => {
+                    if (this.$refs.telInput) {
+                        this.$refs.telInput.focus();
+                        }
+                    });
+                    }else if(document.activeElement.id === "telIpt"){
+                        this.$nextTick(() => {
+                            if (this.$refs.passInput) {
+                                this.$refs.passInput.focus();
+                                }
+                            });
+                    }else if(document.activeElement.id === "passIpt"){
+                        this.$nextTick(() => {
+                            if (this.$refs.checkpassInput) {
+                                this.$refs.checkpassInput.focus();
+                                }
+                            });
+                    }else if(document.activeElement.id === "checkpassIpt"){
+                        this.updateBtn();
+                    }
+                }else{
+                    if(this.successDialogVisible){
+                        this.successDialogVisible = false;
+                    }else if(this.failDialogVisible){
+                        event.preventDefault();
+                        this.failDialogVisible = false;
+                    }else if(this.telDialogVisible){
+                        event.preventDefault();
+                        this.updateData();
+                        this.telDialogVisible = false;
+                    }else if(this.exitDialogVisible){
+                        this.exitDialogVisible = false;
+                        this.exit();
+                    }else if(this.deleteDialogVisible){
+                        event.preventDefault();
+                        this.deleteDialogVisible = false;
+                        this.confirmDialogVisible = true;
+                    }else if(this.confirmDialogVisible){
+                        this.deleteAccount();
+                    }
+                }
+            }
+            
         }
     }
 }
